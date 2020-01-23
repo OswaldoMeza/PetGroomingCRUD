8ab97fd1f5b1519f595cc3dd7a3bcf62999aa8b3
Oswaldo Meza <Oswaldo_Meza@outlook.com>
2020-01-23 10:22:53 -0500

    .

---
+++ PetGrooming/Migrations/202001091936073_good.cs
@@ -0,0 +1,18 @@
+﻿namespace PetGrooming.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class good : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Pets", "GoodBoyorGirl", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Pets", "GoodBoyorGirl");
+        }
+    }
+}
