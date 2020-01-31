d3e549b7ee80d031eccbf55900b6717fec344ec5
Oswaldo Meza <Oswaldo_Meza@outlook.com>
2020-01-30 20:59:40 -0500

    CRUD Commit

--- PetGrooming/Controllers/SpeciesController.cs
+++ PetGrooming/Controllers/SpeciesController.cs
@@ -34,11 +34,74 @@ namespace PetGrooming.Controllers
         }
 
         // Show
+
+        public ActionResult Show(int id)
+        {
+            string query = "select * from species where SpeciesID = @id";
+            SqlParameter parameter = new SqlParameter("@id", id);
+
+            Species selectedspecies = db.Species.SqlQuery(query, parameter).First();
+
+            return View(selectedspecies);
+        }
         // Add
+
+        public ActionResult Add()
+        {
+            return View();
+        }
         // [HttpPost] Add
+
+        [HttpPost]
+        public ActionResult Add(string SpeciesName )
+        {
+            string query = "insert into species (Name) values (@SpeciesName)";
+
+            SqlParameter parameter = new SqlParameter("@SpeciesName", SpeciesName);
+
+            db.Database.ExecuteSqlCommand(query, parameter);
+
+            return RedirectToAction("List");
+
+            //return View();
+        }
         // Update
+
+        public ActionResult Update(int id)
+        {
+            //we need the species name
+            string query = "select * from species where SpeciesID = @id";
+            SqlParameter parameter = new SqlParameter("@id", id);
+
+            Species selectedspecies = db.Species.SqlQuery(query, parameter).FirstOrDefault();
+
+            return View(selectedspecies);
+        }
         // [HttpPost] Update
+        [HttpPost]
+        public ActionResult Update(int id, string SpeciesName)
+        {
+            Debug.WriteLine(SpeciesName + id.ToString());
+            string query = "update species set Name = @SpeciesName where SpeciesID=@id";
+            SqlParameter[] parameters = new SqlParameter[2];
+
+            parameters[0] = new SqlParameter("@id", id);
+            parameters[1] = new SqlParameter("@SpeciesName", SpeciesName);
+
+            db.Database.ExecuteSqlCommand(query, parameters);
+
+            return RedirectToAction("List");
+        }
         // (optional) delete
+
+        public ActionResult Delete(int id)
+        {
+            string query = "delete from species where SpeciesID=@id";
+            SqlParameter parameter = new SqlParameter("@id", id);
+            db.Database.ExecuteSqlCommand(query, parameter);
+
+            return RedirectToAction("List");
+        }
         // [HttpPost] Delete
     }
 }
---
+++ PetGrooming/Models/ViewModels/UpdatePet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetGrooming.Models.ViewModels
+{
+    public class UpdatePet
+    {
+        //What information does update pet need?
+        //needs lost of species and individual pet
+
+        public Pet pet { get; set; }
+        public List<Species> species { get; set; }
+    }
+}
